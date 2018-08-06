94ed46928fff15cea2a362764572bb14e7a107cf
Rajesh Tamang <32210696+siphystar@users.noreply.github.com>
2018-08-06 20:11:25 +0200

    Update 4_soutions.sql

--- 4_soutions.sql
+++ 4_soutions.sql
@@ -7,7 +7,7 @@ SELECT name FROM world
 
 --2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
 
- SELECT name FROM world WHERE gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom');
+ SELECT name FROM world WHERE continent = 'Europe'  AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom');
 
 --3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
 
