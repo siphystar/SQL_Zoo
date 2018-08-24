4d4ad24b5478303b0187a3a14d2eb4e1adb13597
Rajesh Tamang <32210696+siphystar@users.noreply.github.com>
2018-08-24 13:17:43 +0200

    Update 4_soutions.sql

--- 4_soutions.sql
+++ 4_soutions.sql
@@ -47,3 +47,7 @@ GROUP BY continent
 HAVING max(population) <= 25000000) AS x
 ON y.continent = x.continent
 
+--10. Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+SELECT name, continent
+FROM world x
+WHERE population > ALL(SELECT population*3 FROM world y WHERE x.continent = y.continent AND population > 0 AND y.name != x.name)
